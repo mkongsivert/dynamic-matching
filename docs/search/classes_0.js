cc26aca41924e0ed51eedff11c119ef17d841716
mkongsivert <mkongsiv@umd.edu>
2021-05-15 15:34:29 -0400

    documentation pt 2

---
+++ docs/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['agent_11',['Agent',['../classAgent.html',1,'Agent'],['../classmatching_1_1Agent.html',1,'matching.Agent']]]
+];
---
+++ docs/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['market_12',['Market',['../classmatching_1_1Market.html',1,'matching.Market'],['../classMarket.html',1,'Market']]]
+];
